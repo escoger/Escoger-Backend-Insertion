3717169913ed708fabea989dfbccd7f420876181
GAURAV KUMAR <gaurav.kumar8@microfocus.com>
2019-01-19 15:48:15 +0530

    First commit

---
+++ src/main/java/com/escoger/backend/insertion/EscogerBackendInsertionApplication.java
@@ -0,0 +1,14 @@
+package com.escoger.backend.insertion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscogerBackendInsertionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscogerBackendInsertionApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/escoger/backend/insertion/EscogerBackendInsertionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.escoger.backend.insertion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EscogerBackendInsertionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
